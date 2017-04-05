e48b46d40973f3f17cd8595eede6e4e856208842
mwawebstudio <mwawebstudio@gmail.com>
2017-04-05 22:30:27 +0300

    Debug change list
    debug

--- js/directives/closeEditing.js
+++ js/directives/closeEditing.js
@@ -1,14 +1,14 @@
+
 angular.module('app').directive('closeEditing', function () {
 	var KEYS = {
 		DELETE: 46
-	
 	};
 	return {
 		scope: {
 			isEditing: '='
 		},
+
 		link: function (scope, element, attrs) {
-			
 			element.on('keyup', function (e) {
 				if (_.isEqual(e.keyCode, KEYS.DELETE)) {
 					scope.isEditing = false;
